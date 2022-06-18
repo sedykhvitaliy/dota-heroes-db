d283e0a6557d85a3ce2dc6c57fbb609b17a488a7
Sedykh Vitaliy <sedykhvit@gmail.com>
2022-06-18 14:54:42 +0300

    format

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 def select():
     pass
 
+
 def delete():
     pass
+
 
 def update():
     pass
 
+
 def insert():
     pass
 
+
 def user_dialog():
-    
+
     a = int(input('Выберите что вы хотите сделать:\n1.Получить данные\n2.Добавить данные\n3.Обновить данные\n4.Удалить данные\n0.Выйти из программы\n'))
     while True:
         if a == 0:
@@ -19,9 +23,9 @@ def user_dialog():
             if b == 'Да':
                 print('Вы вышли из программы')
                 break
-        elif a>4:
+        elif a > 4:
             break
-        elif a<4:
+        elif a < 4:
             c = int(input('\n1.Герои(Heroes)\n2.Доп.инфо(heroes_info\n3.Характеристики(stats)\n4.Уровень меты(meta)\n9.Вернуться в предыдущее меню\n0.Выйти из программы\n'))
             if c == 0:
                 break
@@ -34,5 +38,7 @@ def user_dialog():
             elif c == 3:
                 name = input('\nВведите имя героя\n')
             elif c == 4:
-                name = input('\nВведите имя героя\n')  
+                name = input('\nВведите имя героя\n')
+
+
 user_dialog()
