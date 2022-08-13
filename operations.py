e11f10789e4127b0edc62a942ddff79ee29d6141
Sedykh Vitaliy <sedykhvit@gmail.com>
2022-08-13 14:16:05 +0300

    #16 refactoring automatic search id

--- operations.py
+++ operations.py
@@ -27,16 +27,6 @@ def insert(file_name: str):
 
     headers = DB_HEADERS.get(file_name)
 
-    with open(file_name, "r", encoding="UTF-8") as file:
-        bd = csv.DictReader(file, delimiter=';')
-        all_lines = list(bd)
-        last_line = (all_lines[-1])
-        last_id = last_line['id']
-        
-        
-        
-            
-
     with open(file_name, "a", encoding="UTF-8", newline='') as file:
     
         i = 1
@@ -46,7 +36,7 @@ def insert(file_name: str):
             print(f'Введите {headers[i]}:')
             user_input = input()
             if user_input.strip():
-                line_dictionary[headers[0]] = int(last_id)+1
+                line_dictionary[headers[0]] = get_last_id(file_name) + 1
                 line_dictionary[headers[i]] = user_input.strip()
                 i += 1
             else:
@@ -72,3 +62,14 @@ def initial_db():
                 break
             DB_HEADERS[table] = headers
             file.close()
+
+def get_last_id(file_name):
+
+    last_id = 0
+    with open(file_name, "r", encoding="UTF-8") as file:
+        bd = csv.DictReader(file, delimiter=';')
+        all_lines = list(bd)
+        last_line = (all_lines[-1])
+        last_id = int(last_line['id'])
+
+    return last_id
