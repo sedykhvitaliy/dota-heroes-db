fef6370eb6901803e10031188df61012bd1d5c42
Sedykh Vitaliy <sedykhvit@gmail.com>
2022-10-08 20:05:09 +0300

    #19 csv loader

---
+++ db/menu_items.py
@@ -0,0 +1,32 @@
+MAIN_TABLE = '.\\bd\heroes.csv'
+META_TABLE = '.\\bd\meta.csv'
+
+
+def get_main_menu():
+    return {
+        1: 'Получить данные',
+        2: 'Добавить данные',
+        3: 'Обновить данные',
+        4: 'Удалить данные',
+        0: 'Выйти из программы'
+    }
+
+
+def get_heroes_menu():
+    return {
+        1: 'Герои(Heroes)',
+        2: 'Доп.инфо(heroes_info)',
+        3: 'Характеристики(stats)',
+        9: 'Вернуться в предыдущее меню',
+        0: 'Выйти из программы'
+    }
+
+
+def get_database():
+    return {
+        1: MAIN_TABLE,
+        2: ".\\bd\heroes_info.csv",
+        3: '.\\bd\stats.csv',
+        4: META_TABLE,
+
+    }
---
+++ db/operations.py
@@ -0,0 +1,70 @@
+import csv
+from menu_items import get_database
+
+DB_HEADERS = {}
+
+
+def select(file_name: str, field_name: str, field_search: str):
+    with open(file_name, "r", encoding="UTF-8") as file:
+        bd = csv.DictReader(file, delimiter=';')
+        for line in bd:
+            if field_search.lower() == line[field_name].lower():
+                return line
+    return {}
+
+
+def delete():
+    pass
+
+
+def update():
+    pass
+
+
+def insert(file_name: str):
+    headers = DB_HEADERS.get(file_name)
+
+    with open(file_name, "a", encoding="UTF-8", newline='') as file:
+
+        i = 1
+        line_dictionary = dict.fromkeys(headers)
+        while i < len(headers):
+
+            print(f'Введите {headers[i]}:')
+            user_input = input()
+            if user_input.strip():
+                line_dictionary[headers[0]] = get_last_id(file_name) + 1
+                line_dictionary[headers[i]] = user_input.strip()
+                i += 1
+            else:
+                print('Введите ещё раз')
+        dictwriter_object = csv.DictWriter(file, fieldnames=headers, delimiter=';')
+        dictwriter_object.writerow(line_dictionary)
+        file.close()
+
+    return print(f'Добавлена запись: {line_dictionary}')
+
+
+def initial_db():
+    database = get_database()
+    headers = []
+    for id in database:
+        table = database.get(int(id))
+        with open(table, "r", encoding="UTF-8") as file:
+            bd = csv.reader(file, delimiter=';')
+            for line in bd:
+                headers = line
+                break
+            DB_HEADERS[table] = headers
+            file.close()
+
+
+def get_last_id(file_name):
+    last_id = 0
+    with open(file_name, "r", encoding="UTF-8") as file:
+        bd = csv.DictReader(file, delimiter=';')
+        all_lines = list(bd)
+        last_line = (all_lines[-1])
+        last_id = int(last_line['id'])
+
+    return last_id
